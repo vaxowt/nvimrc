3ab68772c5a246f9ff90f6f9ecde10a0d15f03d2
vaxowt <vaxowt@gmail.com>
2022-05-22 16:43:21 +0800

    setup gitsigns.nvim mappings

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -1,1 +1,34 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- navigation
+        map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+        end, { expr = true })
+
+        -- actions
+        map('n', '<leader>gp', gs.preview_hunk)
+        map('n', '<leader>gb', function() gs.blame_line { full = true } end)
+        map('n', '<leader>gB', gs.toggle_current_line_blame)
+        map('n', '<leader>gd', gs.diffthis)
+        map('n', '<leader>gD', function() gs.diffthis('~') end)
+
+        -- text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    end
+})
