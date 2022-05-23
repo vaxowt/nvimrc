6a6e9581018ff7f9c5a5c116aa45b8b832283116
vaxowt <vaxowt@gmail.com>
2022-05-23 11:35:56 +0800

    add language server: bashls

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -64,20 +64,14 @@ end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require('lspconfig')['pyright'].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-})
+local servers = { 'pyright', 'clangd', 'bashls', 'texlab' }
 
-require('lspconfig')['texlab'].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-})
-
-require('lspconfig')['clangd'].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-})
+for _, server in pairs(servers) do
+    require('lspconfig')[server].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+    })
+end
 
 require('lspconfig')['sumneko_lua'].setup({
     on_attach = on_attach,
