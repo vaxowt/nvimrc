b2e523ef5c93fc3d32475f455cdbdf04473315d3
vaxowt <vaxowt@gmail.com>
2022-05-31 11:09:36 +0800

    light colorscheme for day

--- after/plugin/config.lua
+++ after/plugin/config.lua
@@ -1,3 +1,8 @@
-vim.cmd([[
-colorscheme everforest
-]])
+local hour = tonumber(os.date('%H'))
+if hour >= 7 and hour < 20 then
+    vim.o.background = 'light'
+    vim.cmd([[colorscheme edge]])
+else
+    vim.o.background = 'dark'
+    vim.cmd([[colorscheme everforest]])
+end
