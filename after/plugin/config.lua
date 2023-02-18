8ca720a76005976596c8c6153557ac99c0c6ab2a
vaxowt <vaxowt@gmail.com>
2023-02-18 21:49:33 +0800

    change colorscheme for day

--- after/plugin/config.lua
+++ after/plugin/config.lua
@@ -1,6 +1,6 @@
 local hour = tonumber(os.date('%H'))
 if hour >= 7 and hour < 20 then
-    vim.cmd([[colorscheme dawnfox]])
+    vim.cmd([[colorscheme edge]])
     vim.o.background = 'light'
 else
     vim.cmd([[colorscheme everforest]])
