402b988fa5f594c732b82ceab99ed8c76ad9a681
vaxowt <vaxowt@gmail.com>
2024-03-30 09:28:48 +0800

    nvim-dap-ui 4.0 requires nvim-nio

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -103,7 +103,7 @@ return {
 
     {
         'rcarriga/nvim-dap-ui',
-        dependencies = { 'mfussenegger/nvim-dap' },
+        dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
         config = get_config('dapui'),
     },
 
