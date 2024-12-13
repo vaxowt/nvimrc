eac79584ec1bbb5aa13f373ed54a22850cec98d4
vaxowt <vaxowt@gmail.com>
2024-12-13 22:30:12 +0800

    fix: 0 is not false

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -367,7 +367,7 @@ return {
 
     {
         'vaxowt/aimswitcher.nvim',
-        enabled = vim.fn.has('win32'),
+        enabled = vim.fn.has('win32') == 1,
         config = get_config('aimswitcher'),
     },
 
