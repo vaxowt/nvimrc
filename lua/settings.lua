09b6c57dd759de46dc78bb8014738768296e60e6
vaxowt <vaxowt@gmail.com>
2025-05-22 23:31:49 +0800

    disable border

--- lua/settings.lua
+++ lua/settings.lua
@@ -5,23 +5,23 @@ vim.filetype.add({
 })
 
 -- change lsp hover border
-local border = {
-    { '┌', 'NormalFloat' },
-    { '─', 'NormalFloat' },
-    { '┐', 'NormalFloat' },
-    { '│', 'NormalFloat' },
-    { '┘', 'NormalFloat' },
-    { '─', 'NormalFloat' },
-    { '└', 'NormalFloat' },
-    { '│', 'NormalFloat' },
-}
+-- local border = {
+--     { '┌', 'NormalFloat' },
+--     { '─', 'NormalFloat' },
+--     { '┐', 'NormalFloat' },
+--     { '│', 'NormalFloat' },
+--     { '┘', 'NormalFloat' },
+--     { '─', 'NormalFloat' },
+--     { '└', 'NormalFloat' },
+--     { '│', 'NormalFloat' },
+-- }
 
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-    opts = opts or {}
-    opts.border = opts.border or border
-    return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
+-- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+-- function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+--     opts = opts or {}
+--     opts.border = opts.border or border
+--     return orig_util_open_floating_preview(contents, syntax, opts, ...)
+-- end
 
 -- change diagnostic signs
 local signs = { Error = '󰅙', Warn = '', Hint = '󰛨', Info = '󰋼' }
