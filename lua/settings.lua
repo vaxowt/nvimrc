92a7b90e2f5b0afdad85b66d9863f56a9aee7c7a
vaxowt <vaxowt@gmail.com>
2025-05-23 22:33:12 +0800

    use native border

--- lua/settings.lua
+++ lua/settings.lua
@@ -4,22 +4,10 @@ vim.filetype.add({
     },
 })
 
--- change lsp hover border
--- local border = {
---     { '┌', 'NormalFloat' },
---     { '─', 'NormalFloat' },
---     { '┐', 'NormalFloat' },
---     { '│', 'NormalFloat' },
---     { '┘', 'NormalFloat' },
---     { '─', 'NormalFloat' },
---     { '└', 'NormalFloat' },
---     { '│', 'NormalFloat' },
--- }
-
 -- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 -- function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 --     opts = opts or {}
---     opts.border = opts.border or border
+--     opts.border = opts.border or 'rounded'
 --     return orig_util_open_floating_preview(contents, syntax, opts, ...)
 -- end
 
