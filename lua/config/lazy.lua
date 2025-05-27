8814abd51770f313bfa11b84fbbb3b6bfd6cfcaa
vaxowt <vaxowt@gmail.com>
2025-05-27 22:55:00 +0800

    disable config change detection

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,9 +27,7 @@ require('lazy').setup({
         -- import your plugins
         { import = 'plugins' },
     },
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
-    -- install = { colorscheme = { 'habamax' } },
+    change_detection = { enabled = false },
     -- automatically check for plugin updates
     checker = { enabled = false },
 })
