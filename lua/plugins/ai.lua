68bc77007e5cc20baf5a11745dab835d9e90c604
vaxowt <vaxowt@termux-chroot-arch>
2025-05-28 17:41:14 +0800

    add: codecompanion.nvim

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        'zbirenbaum/copilot.lua',
+        opts = {},
+    },
+    {
+        'olimorris/codecompanion.nvim',
+        opts = {},
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-treesitter/nvim-treesitter',
+        },
+    },
+}
