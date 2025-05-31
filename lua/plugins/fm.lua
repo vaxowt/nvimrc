5ba95a747d13d9a695843cb9613b3897d3406de6
vaxowt <vaxowt@gmail.com>
2025-05-31 19:40:40 +0800

    feat(plugins): add yazi.nvim for improved file management
    Added `yazi.nvim` to address limitations in file system operations with existing Neovim file managers like `lir.nvim`.

--- lua/plugins/fm.lua
+++ lua/plugins/fm.lua
@@ -184,4 +184,41 @@ return {
             'NvimTreeFindFileToggle',
         },
     },
+
+    {
+        'mikavilpas/yazi.nvim',
+        event = 'VeryLazy',
+        dependencies = {
+            -- check the installation instructions at
+            -- https://github.com/folke/snacks.nvim
+            'folke/snacks.nvim',
+        },
+        keys = {
+            {
+                '<leader>-',
+                '<cmd>Yazi<cr>',
+                mode = { 'n', 'v' },
+                desc = 'Open yazi at the current file',
+            },
+            {
+                -- Open in the current working directory
+                '<leader>cw',
+                '<cmd>Yazi cwd<cr>',
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                '<c-up>',
+                '<cmd>Yazi toggle<cr>',
+                desc = 'Resume the last yazi session',
+            },
+        },
+        ---@type YaziConfig | {}
+        opts = {
+            -- use lir.nvim instead
+            open_for_directories = false,
+            keymaps = {
+                show_help = '?',
+            },
+        },
+    },
 }
