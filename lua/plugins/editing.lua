9d206cdff37ceff4bca6b2ca09fbb8fd33a715cd
vaxowt <vaxowt@gmail.com>
2025-05-31 20:57:35 +0800

    feat: replace nrpattern.nvim with dial.nvim due to lack of updates
    The nrpattern.nvim plugin has been replaced with dial.nvim, as nrpattern.nvim has not been actively maintained for a long time. Dial.nvim offers enhanced increment/decrement functionality with support for various formats such as integers, dates, booleans, constants, and hex colors. Key mappings for increment/decrement actions have also been added.

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -43,16 +43,73 @@ return {
     -- 『盘古之白』中文排版自动规范化的 Vim 插件
     { 'hotoo/pangu.vim', cmd = 'Pangu' },
 
-    -- Neovim plugin to expand incrementing/decrementing to more formats.
+    -- enhanced increment/decrement plugin for Neovim.
     {
-        'zegervdv/nrpattern.nvim',
+        'monaqa/dial.nvim',
+        keys = {
+            { '<C-a>', '<Plug>(dial-increment)', mode = { 'n', 'v' } },
+            { '<C-x>', '<Plug>(dial-decrement)', mode = { 'n', 'v' } },
+            { 'g<C-a>', 'g<Plug>(dial-increment)', mode = { 'n', 'v' }, remap = true },
+            { 'g<C-x>', 'g<Plug>(dial-decrement)', mode = { 'n', 'v' }, remap = true },
+        },
         config = function()
-            local patterns = require('nrpattern.default')
-
-            patterns[{ 'True', 'False' }] = { priority = 5 }
-            patterns[{ 'yes', 'no' }] = { priority = 5 }
-
-            require('nrpattern').setup(patterns)
+            local augend = require('dial.augend')
+            require('dial.config').augends:register_group({
+                -- default augends used when no group name is specified
+                default = {
+                    augend.integer.alias.decimal,
+                    -- augend.integer.alias.decimal_int, -- including negative number
+                    augend.integer.alias.hex,
+                    augend.integer.alias.octal,
+                    augend.integer.alias.binary,
+                    augend.date.alias['%Y/%m/%d'],
+                    augend.date.alias['%m/%d/%Y'],
+                    augend.date.alias['%d/%m/%Y'],
+                    augend.date.alias['%m/%d/%y'],
+                    augend.date.alias['%d/%m/%y'],
+                    augend.date.alias['%m/%d'],
+                    augend.date.alias['%-m/%-d'],
+                    augend.date.alias['%Y-%m-%d'],
+                    augend.date.alias['%d.%m.%Y'],
+                    augend.date.alias['%d.%m.%y'],
+                    augend.date.alias['%d.%m.'],
+                    augend.date.alias['%-d.%-m.'],
+                    augend.date.alias['%Y年%-m月%-d日'],
+                    augend.date.alias['%Y年%-m月%-d日(%ja)'],
+                    augend.date.alias['%H:%M:%S'],
+                    augend.date.alias['%H:%M'],
+                    augend.constant.alias.bool,
+                    augend.constant.alias.alpha,
+                    augend.constant.alias.Alpha,
+                    augend.semver.alias.semver,
+                    augend.constant.new({
+                        elements = { 'True', 'False' },
+                        word = true,
+                        cyclic = true,
+                    }),
+                    augend.constant.new({
+                        elements = { 'yes', 'no' },
+                        word = true,
+                        cyclic = true,
+                    }),
+                    augend.constant.new({
+                        elements = { 'and', 'or' },
+                        word = true,
+                        cyclic = true,
+                    }),
+                    augend.constant.new({
+                        elements = { '&&', '||' },
+                        word = false,
+                        cyclic = true,
+                    }),
+                    augend.constant.new({
+                        elements = { '&', '|' },
+                        word = false,
+                        cyclic = true,
+                    }),
+                    augend.hexcolor.new({ case = 'prefer_upper' }),
+                },
+            })
         end,
     },
 
