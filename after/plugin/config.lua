278499ca7304e962123f85074f361798054911de
vaxowt <vaxowt@gmail.com>
2025-06-01 12:01:28 +0800

    fix(config): move colorscheme logic to options.lua to resolve terminal color issues
    The colorscheme logic was moved from `after/plugin/config.lua` to `lua/config/options.lua` to address terminal color inconsistencies. The `config.lua` file has been deleted as its functionality is now integrated into `options.lua`.

--- after/plugin/config.lua
+++
@@ -1,8 +0,0 @@
-local hour = tonumber(os.date('%H'))
-if hour >= 7 and hour < 20 then
-    vim.o.background = 'light'
-    vim.cmd([[colorscheme edge]])
-else
-    vim.o.background = 'dark'
-    vim.cmd([[colorscheme everforest]])
-end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -67,3 +67,12 @@ vim.o.fileencodings = 'ucs-bom,utf-8,gbk,big5,gb18030,utf-16,latin1'
 -- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+local hour = tonumber(os.date('%H'))
+if hour >= 7 and hour < 20 then
+    vim.o.background = 'light'
+    vim.cmd.colorscheme('edge')
+else
+    vim.o.background = 'dark'
+    vim.cmd.colorscheme('everforest')
+end
