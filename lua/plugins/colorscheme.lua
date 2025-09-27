5e6843ecc5d6f8b2d6623e42784ae9e226efd5e5
vaxowt <vaxowt@gmail.com>
2025-09-27 08:57:52 +0800

    feat(colorscheme): set Everforest float window style to 'none'

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -47,6 +47,7 @@ return {
             vim.g.everforest_sign_column_background = 'none'
             vim.g.everforest_better_performance = 1
             vim.g.everforest_enable_italic = 1
+            vim.g.everforest_float_style = 'none'
         end,
     },
 
