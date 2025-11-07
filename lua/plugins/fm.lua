32a9872f4cc1448d0331148e765a045a77ebd6c0
vaxowt <vaxowt@gmail.com>
2025-11-07 22:24:45 +0800

    feat(oil): set rounded border for floating window
    Configure oil.nvim's floating window to use a 'rounded' border for improved visual consistency.

--- lua/plugins/fm.lua
+++ lua/plugins/fm.lua
@@ -40,6 +40,7 @@ return {
             float = {
                 max_width = 0.8,
                 max_height = 0.8,
+                border = 'rounded',
             },
         },
         dependencies = {
